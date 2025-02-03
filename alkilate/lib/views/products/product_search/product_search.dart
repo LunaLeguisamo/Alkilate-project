cb4f6d0c31cca9cc5403988378b34483c01d9155
hexNulgath <machadoignacio@hotmail.com>
2025-02-03 16:59:24 -0300

    Implement routing and initial screens for the ALkilate app

---
+++ alkilate/lib/views/products/product_search/product_search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductSearchScreen extends StatelessWidget {
+  const ProductSearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
