3f2a0facefbc802a7f012263e6def1dc5f3256b2
hexNulgath <machadoignacio@hotmail.com>
2025-02-04 18:51:51 -0300

    Update HomeScreen layout and add SearchBar component

--- alkilate/lib/views/home/home.dart
+++ alkilate/lib/views/home/home.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatelessWidget {
                   height: 154,
                   fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 21),
                 const Text(
                   'Welcome to',
                   style: TextStyle(
@@ -37,22 +37,67 @@ class HomeScreen extends StatelessWidget {
                   fit: BoxFit.cover,
                   alignment: Alignment.center,
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 70),
+                Container(
+                  height: 30,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: SearchBar(
+                    leading: const Icon(Icons.search),
+                    trailing: <Widget>[
+                      Tooltip(
+                        message: 'Filter',
+                        child: Icon(
+                          Icons.filter_list_outlined,
+                          color: Color(0xFF1B1B1B),
+                        ),
+                      )
+                    ],
+                    hintText: 'Search',
+                    hintStyle: WidgetStateProperty.all(
+                        TextStyle(color: Color(0xFF808080))),
+                    backgroundColor: WidgetStateProperty.all(Colors.white),
+                    shadowColor: WidgetStateProperty.all(Color(0xFF808080)),
+                    elevation: WidgetStateProperty.all(1),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
               ],
             ),
-            Container(
-              width: double.infinity, // This makes the container take max width
-              color: Colors.grey[200], // Optional: for visual debugging
+            SizedBox(
+              width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
-                    'About Us',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600, // Semi-bold
-                      fontSize: 34,
-                      letterSpacing: -0.03,
-                      color: Color(0xFF1B1B1B),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: Text(
+                      'About Us',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 34,
+                        letterSpacing: -0.03,
+                        color: Color(0xFF1B1B1B),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                      'Vestibulum nec ultricies nunc. Nullam in purus nec nulla '
+                      'vulputate lacinia. Nulla facilisi. Nullam in purus nec nulla '
+                      'vulputate lacinia. Nulla facilisi.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color(0xFF000000),
+                      ),
                     ),
                   ),
                 ],
