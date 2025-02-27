af25d8c4ce03c9591ab1ab71dad26d96fb0ab9d5
LunaLeguisamo <9356@holbertonstudents.com>
2025-02-26 23:45:12 -0300

    ia

--- alkilate/AI/ai.py
+++ alkilate/AI/ai.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
-
-app = Flask(__name__)
+import requests
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -17,26 +15,12 @@ else:
     print(f'Error loading products: {response.status_code}')
     products = []
 
-@app.route('/suggestions', methods=['POST'])
-def get_suggestions():
-    query = request.json.get('query', '')
-    
-    if not query:
-        return jsonify({"error": "No query provided"}), 400
-    
-    suggested_products = suggestions(products, query)
-    
-    return jsonify(suggested_products)
-
 def suggestions(products, query, top_n=10):
-    info = [f"{product['name']} {product['category']} {product['description']}" for product in products]
-    
+    info = [f"{product['name']} {product['category']}" for product in products]
     vectors_info = model.encode(info, convert_to_tensor=True)
-    vectos_query = model.encode(query, convert_to_tensor=True)
-    print(vectors_info)
+    vectors_query = model.encode(query, convert_to_tensor=True)
     
-    similarities = util.pytorch_cos_sim(vectos_query, vectors_info)[0].cpu().numpy()
-    print(similarities)
+    similarities = util.pytorch_cos_sim(vectors_query, vectors_info)[0].cpu().numpy()
     
     similar_indices = np.argsort(similarities)[::-1][:top_n]
     
@@ -45,7 +29,7 @@ def suggestions(products, query, top_n=10):
     suggested_products = [products[i] for i in similar_indices if similarities[i] > umbral]
     return suggested_products
 
-query = "I will make a birthday"
+query = "tool"
 suggested_products = suggestions(products, query, 10)
 
 if suggested_products:
@@ -55,5 +39,3 @@ if suggested_products:
 else:
     print('No suggestions found')
     
-if __name__ == '__main__':
-    app.run(debug=True)
